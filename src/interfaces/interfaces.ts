77f09f5092b37929b626f1c8895a96179f0c5bcb
pablo-gorgoglione <pablogorgoglione@gmail.com>
2022-01-05 20:19:13 -0300

    fix: backend url...

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,9 +1,9 @@
 //interfaces for contexts and providers
 
-export const urlBACKEND: string = 'http://localhost:4000';
-/* export const urlBACKEND: string =
+// export const urlBACKEND: string = 'http://localhost:4000';
+export const urlBACKEND: string =
   'https://blog-api-pablogorgoglione.herokuapp.com';
- */
+
 export interface IPostState {
   posts: IPost[];
 }
